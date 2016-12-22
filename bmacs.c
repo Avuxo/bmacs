5ca9be0626bd1c15142b819ed368dfbb9c19f3e1
Ben <ben@july7.pw>
2016-12-22 14:11:27 -0500

    small things, changing whitespace, comments etc

--- bmacs.c
+++ bmacs.c
@@ -9,22 +9,23 @@ int main(int argc, char **argv){
 
     initscr(); /*start ncurses*/
     
-    FILE *fp;
-    fp = fopen(argv[1], "w"); /*open up the file in the first command line argument*/
-    
+    FILE *file;
+    file = fopen(argv[1], "w"); /*open up the file in the first command line argument*/
+
     //main program loop
     int exitFlag = 0;
     do{
         //get the user inputed character
-        int ch = getch();
-
-        if(strcmp(keyname(ch), "^R") == 0){ //ctrl+r exits the program and saves
+        int input = getch();
+        
+        if(strcmp(keyname(input), "^R") == 0){ //ctrl+r exits the program and saves
             exitFlag = 1;
         }
+        
     }while(exitFlag == 0);
 
-    fprintf(fp,"test"); /*write to the file specified in argv[1]*/
-    fclose(fp);         /*close the file output stream*/
+    fprintf(file,"test"); /*write to the file specified in argv[1]*/
+    fclose(file);         /*close the file output stream*/
     endwin();           /*exit ncurses*/
     return 0;
 }
@@ -36,6 +37,5 @@ void usage(void){
 
 /*
   bmacs version 0.1
-  https://github.com/asonskii/bmacs
-
+  https://github.com/asonskii/bmacs.git
 */
