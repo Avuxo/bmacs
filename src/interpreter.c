99a6bbc03dc80ceb42f6709c3870549330b36007
Ben <ben@july7.pw>
2017-05-10 21:17:04 -0400

    add in working registers

--- src/interpreter.c
+++ src/interpreter.c
@@ -49,23 +49,44 @@ void interpret(Buffer *buf, char *commands){
 void command(Buffer *buf, const char *cmd, int index){
     /*boolean -- does command have integer arguments?*/
     int hasInts = nextIsInt(cmd, index);
+    printf("hasInts? %d\n", hasInts);
     /*check each char for command*/
     switch(cmd[index]){
     case '>': /*buffer move right x chars (where x is an integer)*/
-        if(!hasInts){
+        if(!nextIsInt(cmd, index+1)){
             printf("Error, expected integer after '> (c:%d)\n", index);
             exit(1);
         }else{
-            moveBufferRight(&(*buf), cmd[index+1] - '0');
-            break;
+            if(!nextIsRegister(cmd, index)){
+                moveBufferRight(&(*buf), cmd[index+1] - '0');
+                break;
+            }else if(cmd[index+1] == '$'){
+                moveBufferLeft(&(*buf), reg1);
+                break;
+            }else if(cmd[index+1] == '&'){
+                moveBufferLeft(&(*buf), reg2);
+                break;
+            }
         }
     case '<': /*buffer move left x chars (where x is an integer)*/
-        if(!hasInts){
+        printf("%c\n", cmd[index+1]);
+        if(!nextIsInt(cmd, index+1)){
             printf("Error, expected integer after '<' (c: %d)\n", index);
             exit(1);
         }else{
-            moveBufferLeft(&(*buf), cmd[index+1] -'0');
-            break;
+            if(nextIsRegister(cmd, index) == 0){
+                moveBufferLeft(&(*buf), cmd[index+1] -'0');
+                break;
+            }else if(cmd[index+1] == '$'){
+                moveBufferLeft(&(*buf), reg1);
+                break;
+            } else if(cmd[index+1] == '&'){
+                moveBufferLeft(&(*buf), reg2);
+                break;
+            }else{
+                printf("unknown error\n");
+                break;
+            }
         }
     case '^': /*character insert*/
         /*using character register?*/
@@ -82,15 +103,17 @@ void command(Buffer *buf, const char *cmd, int index){
         break;
     /*loop start*/
     case '[':{
-        if(!hasInts){
-            if(nextIsRegister(cmd, index)){
-                printf("Expected integer or register after '[ (c: %d)\n'", index);
-                exit(1);
-            }
+        if(!hasInts && !nextIsRegister(cmd, index)){
+            printf("Expected integer or register after '[' (c: %d)\n", index);
+            exit(1);
         }else{
+            int numLoop;
             /*actual integer*/
-
-            int numLoop = cmd[index+1] - '0';
+            if(nextIsInt(cmd, index)) numLoop = cmd[index+1] - '0';
+            else if(nextIsRegister(cmd, index) == 1){
+                if(cmd[index+1] == '$') numLoop = reg1;
+                else if(cmd[index+1] == '&') numLoop = reg2;
+            }
             for(int i=1; i<numLoop; i++){
                 int j=1;
                 /*go until loop close*/
@@ -99,8 +122,6 @@ void command(Buffer *buf, const char *cmd, int index){
                     j++;
                 }
             }
-               
- 
             break;
         }
     }
@@ -117,11 +138,13 @@ void command(Buffer *buf, const char *cmd, int index){
         decrementReg(cmd[index+1]);
         break;
     case '=':
+        printf("assigning reg: %c to %c\n", cmd[index+1], cmd[index+2]);
         /*integer register?*/
         if(nextIsRegister(cmd, index) == 1){
             /*is the next character after the register an integer?*/
             if(nextIsInt(cmd, index+1)){
-                setIntRegister(cmd[index+1], cmd[index+2]);
+                setIntRegister(cmd[index+1], cmd[index+2] - '0');
+                break;
             }else{
                 break;
             }
@@ -130,6 +153,7 @@ void command(Buffer *buf, const char *cmd, int index){
             /*is the next character after the register a character*/
             if(nextIsChar(cmd, index+1)){
                 setCharRegister(cmd[index+1], cmd[index+2]);
+                break;
             }else{
                 break;
             }
@@ -149,6 +173,7 @@ int nextIsInt(const char *cmd, int index){
     const char *integers = "0123456789$&";
     for(int i=0; i<strlen(integers); i++){
         if(cmd[index] == integers[i]){
+            printf("yes . is int\n");
             return 1; //TODO: recur to find number of integers after (for more than 1 digit)
         }
     }
@@ -171,12 +196,12 @@ int nextIsRegister(const char *cmd, int index){
     const char *intRegisters = "$&";
     const char *charRegisters = "#";
     for(int i=0; i<strlen(intRegisters); i++){
-        if(cmd[index] == intRegisters[i]){
+        if(cmd[index+1] == intRegisters[i]){
             return 1;
         }
     }
     for(int i=0; i<strlen(charRegisters); i++){
-        if(cmd[index] == charRegisters[i]){
+        if(cmd[index+1] == charRegisters[i]){
             return 2;
         }
     }
@@ -185,11 +210,10 @@ int nextIsRegister(const char *cmd, int index){
 
 /*set given register to the given value (integer type)*/
 void setIntRegister(char reg, int value){
-    if(reg != '$' || reg != '&') return;
-    if(reg == '$') reg1 = value;
+    if(reg != '$' && reg != '&') return;
+    else if(reg == '$') reg1 = value;
     else if(reg == '&') reg2 = value;
 }
-
 /*set given register to the given value (char type)*/
 void setCharRegister(char reg, char value){
     if(reg != '#') return;
